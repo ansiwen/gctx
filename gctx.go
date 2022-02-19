5156caf4c916e42c3ddb82013f556263042be2c9
Sven Anderson <sven@anderson.de>
2022-02-19 20:33:44 +0100

    improve comments
    Signed-off-by: Sven Anderson <sven@anderson.de>

--- gctx.go
+++ gctx.go
@@ -1,4 +1,6 @@
-// Package gctx implements a goroutine context.
+// Package gctx implements a goroutine context (gctx). A gctx is a non-global
+// context that is linked to one specific goroutine and can be retrieved from
+// any scope within that goroutine.
 package gctx
 
 import (
@@ -6,7 +8,10 @@ import (
 	"runtime/pprof"
 )
 
-// Do calls f with ctx set as the current goroutine context.
+// Do calls f with ctx set as the current goroutine's gctx. Goroutines created
+// within f inhert the gctx, but it is only available as long as f didn't
+// return. If this is not sufficient, because the child outlives f, the child
+// has to create its own gctx, which can be based on the gctx of f.
 func Do(ctx context.Context, f func()) {
 	oldLabels := runtimeGetProfLabel()
 	id := newID()
@@ -18,7 +23,8 @@ func Do(ctx context.Context, f func()) {
 	delete(id)
 }
 
-// Get retrieves the current goroutine context.
+// Get retrieves the current goroutines gctx. It returns nil if no gctx has been
+// created, or it has already been deleted.
 func Get() context.Context {
 	return get(currentID())
 }
