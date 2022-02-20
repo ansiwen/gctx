6f66587fd4b10b75e65852123746863cbe9ffa91
Sven Anderson <sven@anderson.de>
2022-02-20 18:38:39 +0100

    add example
    Signed-off-by: Sven Anderson <sven@anderson.de>

--- gctx_test.go
+++ gctx_test.go
@@ -2,6 +2,7 @@ package gctx_test
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"testing"
 
@@ -40,4 +41,41 @@ func TestGctx(t *testing.T) {
 		wg.Done()
 	})
 	wg.Wait()
+}
+
+type writer func(p []byte) (n int, err error)
+
+func (w writer) Write(p []byte) (n int, err error) {
+	return w(p)
+}
+
+func Example_logger() {
+	type requestID struct{}
+	// "global" logger instance
+	log := func(s string) {
+		rid, _ := gctx.Get().Value(requestID{}).(int)
+		fmt.Printf("[LOG] reqID: %d - %s\n", rid, s)
+	}
+	// placeholder for an external function without context support doing an
+	// arbitrary request and using a global logger
+	createSomeResource := func() {
+		log("sent create request")
+	}
+	var wg sync.WaitGroup
+	// asynchronously request 5 times to create some resource
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		ctx := context.WithValue(context.Background(), requestID{}, i)
+		go gctx.Do(ctx, func() {
+			createSomeResource()
+			wg.Done()
+		})
+	}
+	wg.Wait()
+	// Unordered output:
+	// [LOG] reqID: 0 - sent create request
+	// [LOG] reqID: 1 - sent create request
+	// [LOG] reqID: 2 - sent create request
+	// [LOG] reqID: 3 - sent create request
+	// [LOG] reqID: 4 - sent create request
 }
